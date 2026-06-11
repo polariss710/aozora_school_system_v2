46bebcae4a2ee57ecbaad6d08b941c0f98df59e0
polariss710 <polariss710@gmail.com>
2026-06-11 10:01:36 +0900

    feat: export teacher wage duty reports

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ export const SUPABASE_CONFIG = {
   anonKey: "sb_publishable_6c7EFHXfq256rvv8KvY0Yw_FrAZtb6x",
 };
 
-export const APP_VERSION = "v2.83.1-student-line-rc-20260610";
+export const APP_VERSION = "v2.85.0-wage-duty-report-export-20260611";
 
 export const PAYMENT_MONTH_FILTER_YEAR_RANGE = {
   start: 2024,
